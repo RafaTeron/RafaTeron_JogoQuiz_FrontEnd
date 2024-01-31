1bad94145ea8d7ce01fe53510c667ac0d9fc0537
RafaTeron Abreu <rafael_sabreu@hotmail.com>
2024-01-31 12:41:16 +0000

    Criado Component Quiz

---
+++ JogoQuiz_FrontEnd/src/app/Quiz/quiz-footer/quiz-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-footer',
+  templateUrl: './quiz-footer.component.html',
+  styleUrl: './quiz-footer.component.css'
+})
+export class QuizFooterComponent {
+
+}
---
+++ JogoQuiz_FrontEnd/src/app/Quiz/quiz-root/quiz-root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-root',
+  templateUrl: './quiz-root.component.html',
+  styleUrl: './quiz-root.component.css'
+})
+export class QuizRootComponent {
+
+}
