1472e362f263d6ed2b8c32da455fcdb6b663e5a2
RafaTeron <rafael_sabreu@hotmail.com>
2024-05-19 22:28:46 -0300

    Finish Score(Parte 1)

--- JogoQuiz_FrontEnd/src/app/component/Finish/finish-painel/finish-painel.component.ts
+++ JogoQuiz_FrontEnd/src/app/component/Finish/finish-painel/finish-painel.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Player } from '../../../model/player';
+import { PlayerService } from '../../../service/Player/player.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-finish-painel',
   templateUrl: './finish-painel.component.html',
   styleUrl: './finish-painel.component.css'
 })
-export class FinishPainelComponent {
+export class FinishPainelComponent implements OnInit {
 
+  id: number;
+  selectedOption!: number;
+  playerFindById: Observable<Player>;
+  timeoutId: any;
+  answerColors: { [key: number]: string } = {};
+
+  constructor(private playerService: PlayerService, private router: Router) {
+    this.id = 1;
+    this.playerFindById = this.playerService.findById(this.id);
+  }
+
+  ngOnInit(): void {
+  }
 }
