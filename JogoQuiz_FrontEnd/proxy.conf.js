f9c1bd995f58b4c7e52b2bc782008e47ee2ed76d
RafaTeron <112910848+RafaTeron@users.noreply.github.com>
2024-11-27 15:01:14 -0300

    Update proxy.conf.js

--- JogoQuiz_FrontEnd/proxy.conf.js
+++ JogoQuiz_FrontEnd/proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: ['/auth', '/app-quiz'],
-    target:'https://deploy-gateway-service-production.up.railway.app',
+    target:'https://deploy-gateway-service-production.up.railway.app/',
     secure: false,
     logLevel: 'debug'
   }
